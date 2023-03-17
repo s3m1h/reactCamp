b47b965efd6f546c8dc7efbd4290ce8cdcac6556
s3m1h <semih_acar01@hotmail.com>
2023-03-17 16:56:34 +0300

    first commit

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -7,7 +7,6 @@ export default function Navi() {
                 <Container>
                     <Menu.Item
                         name='home'
-
                     />
                     <Menu.Item
                         name='messages'
